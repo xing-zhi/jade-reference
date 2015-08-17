7c8880b7d24b0447c15473553c34363710bc6df8
xingzhi <xing_zhi@126.com>
2015-08-17 16:21:27 +0800

    尝试最小化js文件，但是uglify对es6特性支持不够，所以暂时不进行最小化

--- gulp-tasks/browserify.js
+++ gulp-tasks/browserify.js
@@ -1,10 +1,15 @@
 'use strict';
 
-module.exports = function browserify(gulp, $, cfg) {
-  return gulp
-    .src(cfg.src)
-    .pipe($.browserify({
-      debug: true
-    }))
+const source = require('vinyl-source-stream'),
+      buffer = require('vinyl-buffer'),
+      browserify = require('browserify');
+
+module.exports = function (gulp, $, cfg) {
+  return browserify(cfg.src)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+// uglify对es6特性的支持不够，解析错误，故暂时不使用
+//    .pipe($.uglify().on('error', $.util.log))
     .pipe(gulp.dest(cfg.dest));
 };
