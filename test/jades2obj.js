1761b7af6844094662cd12230f0d50165c7ba4c1
xingzhi <xing_zhi@126.com>
2015-08-19 17:51:20 +0800

    add test

---
+++ test/jades2obj.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const fs = require('fs'),
+      path = require('path');
+
+const test = require('tape'),
+      jade = require('jade');
+
+const jades2obj = require('../gulp-tasks/jades2obj.js');
+
+const jadesPath = './test/test-data/jade-snippets';
+
+function clearGeneratedData(filePath) {
+  fs.unlink(filePath);
+}
+
+test('jades2obj is a function', function(t) {
+  t.equal(typeof jades2obj, 'function');
+
+  t.end();
+});
+
+test('just concat jade files to a module. ', function(t) {
+  const filePath = './test/test-data/jades.js';
+
+  jades2obj(jadesPath, filePath, 'intact');
+
+  const jadesObj = require('./test-data/jades');
+
+  t.equal(typeof jadesObj, 'object');
+
+  Object.keys(jadesObj).forEach(function(key) {
+    const file = path.join(__dirname, `./test-data/jade-snippets/${key}.jade`);
+
+    fs.readFile(file, 'utf-8', function(err, content) {
+      if ( err ) {
+        console.error(err);
+        t.fail();
+      }
+      t.equal(jadesObj[key], content);
+    });
+  });
+
+  clearGeneratedData(filePath);
+
+  t.end();
+});
+
+test('compile jade files and concat the result to a module', function(t) {
+  const filePath = './test/test-data/templates.js';
+
+  jades2obj(jadesPath, filePath, 'compile', {highlight: false});
+
+  const templatesObj = require('./test-data/templates');
+
+  t.equal(typeof templatesObj, 'object');
+
+  Object.keys(templatesObj).forEach(function(key) {
+    const file = path.join(jadesPath, `${key}.jade`);
+
+    fs.readFile(file, 'utf-8', function(err, content) {
+
+      const template = jade.compileClient(content, {
+        debug: false,
+        compileDebug: true,
+        filename: file,
+        doctype: 'html'
+      }).trim();
+
+      t.equal(templatesObj[key], template);
+    });
+  });
+
+  clearGeneratedData(filePath);
+  t.end();
+});
+
+test('render jade files and concat the result to a module', function(t) {
+  const filePath = './test/test-data/htmls.js';
+
+  jades2obj(jadesPath, filePath, 'render', {highlight: false});
+
+  const htmlsObj = require('./test-data/htmls');
+
+  t.equal(typeof htmlsObj, 'object');
+
+  Object.keys(htmlsObj).forEach(function(key) {
+    const file = path.join(jadesPath, `${key}.jade`);
+
+    fs.readFile(file, 'utf-8', function(err, content) {
+
+      const html = jade.render(content, {
+        debug: false,
+        compileDebug: true,
+        filename: file,
+        pretty: true,
+        doctype: 'html'
+      }).trim();
+
+      t.equal(htmlsObj[key], html);
+    });
+  });
+
+  clearGeneratedData(filePath);
+  t.end();
+});
