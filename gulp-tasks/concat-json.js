6ebb8ef32ba23f223c736dc7f95da48db6d8bd20
xingzhi <xing_zhi@126.com>
2015-08-20 09:45:39 +0800

    修改“concat-json”，更易读、同步一致性

--- gulp-tasks/concat-json.js
+++ gulp-tasks/concat-json.js
@@ -7,7 +7,8 @@ function concatJson(filePath, outFile) {
   const files = fs.readdirSync(filePath);
 
   let json = files.reduce(function file2obj(finalJson, filename) {
-    const content = fs.readFileSync(path.join(__dirname, '../', filePath, filename), 'utf-8');
+    const file = path.join(filePath, filename);
+    const content = fs.readFileSync(file, 'utf-8');
     const obj = JSON.parse(content);
 
     return Object.keys(obj).reduce(function stringify(tmpJson, key) {
@@ -19,7 +20,7 @@ function concatJson(filePath, outFile) {
 
   json = JSON.stringify(json);
 
-  fs.writeFile(path.join(__dirname, '../', outFile), json);
+  fs.writeFileSync(outFile, json);
 }
 
 module.exports = concatJson;
