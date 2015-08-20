50c9fcc203dc30edd7383bb30857d18c4e83db02
xingzhi <xing_zhi@126.com>
2015-08-20 14:41:21 +0800

    sync -> async: files2obj.js

--- gulp-tasks/files2obj.js
+++ gulp-tasks/files2obj.js
@@ -5,18 +5,64 @@ const path = require('path'),
 
 function files2obj(folderName, outFile) {
   const obj = {};
-  const files = fs.readdirSync(folderName);
 
-  files.forEach(function file2property(filename) {
-    const filePath = path.join(folderName, filename),
-          fileContent = fs.readFileSync(filePath, 'utf-8');
+  const readdir = new Promise(function(res, rej) {
+    fs.readdir(folderName, function(err, files) {
+      if ( err ) {
+        rej(err);
+      }
 
-    obj[filename] = fileContent;
+      res(files);
+    });
   });
 
-  const outCome = `module.exports = ${JSON.stringify(obj)};`;
+  function readFiles(files) {
+    const promises = [];
 
-  fs.writeFileSync(outFile, outCome);
+    files.forEach(function(file) {
+      const filePath = path.join(folderName, file);
+      const promise = new Promise(function(res, rej) {
+        fs.readFile(filePath, 'utf-8', function(err, content) {
+          if ( err ) {
+            rej(err);
+          }
+
+          res([file, content]);
+        });
+      });
+
+      promises.push(promise);
+    });
+
+    return Promise.all(promises);
+  }
+
+  function writeFile(contentArrs) {
+    let obj = contentArrs.reduce(function(finalObj, contentArr) {
+      finalObj[contentArr[0]] = contentArr[1];
+
+      return finalObj;
+    }, {});
+
+    obj = `module.exports = ${JSON.stringify(obj)};`;
+
+    const write = new Promise(function(res, rej) {
+      fs.writeFile(outFile, obj, function(err) {
+        if ( err ) {
+          rej(err);
+        }
+
+        res();
+      });
+    });
+
+    return write;
+  }
+
+  return readdir
+    .then(readFiles)
+    .then(writeFile)
+    .catch(console.error);
 }
 
 module.exports = files2obj;
